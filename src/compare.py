08e22200ac72a21c650ded6bcac5051afa25e593
marco-botte <marco.botte90@googlemail.com>
2020-03-22 00:51:13 +0100

    Add compare

---
+++ src/compare.py
@@ -0,0 +1,11 @@
+from .schedulers.simple_scheduler import SimpleScheduler
+from .schedulers.capacity_coefficient_scheduler import CapacityScheduler
+from .simulate import simulate
+
+
+def compare(instance):
+    simple_scheduler = SimpleScheduler()
+    capacity_scheduler = CapacityScheduler()
+
+    simulate(instance, simple_scheduler)
+    simulate(instance, capacity_scheduler)
