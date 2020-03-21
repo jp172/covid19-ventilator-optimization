f869ef2d70d375115d60dc103a2c9284b886fe5d
marco-botte <marco.botte90@googlemail.com>
2020-03-21 14:50:07 +0100

    Remove drivers from project

--- src/schedulers/simple_scheduler.py
+++ src/schedulers/simple_scheduler.py
@@ -3,7 +3,7 @@ from ..helper_functions.coordinates_to_distance import get_distance
 
 class SimpleScheduler:
     # todo: on given all the objects, return the most suitable hospital ID
-    def assign_request_to_hospital(self, hospitals, vehicles, drivers, request):
+    def assign_request_to_hospital(self, hospitals, vehicles, request):
         # this takes just the nearest hospital
         best_hospital = min(
             hospitals.values(), key=lambda h: get_distance(h.position, request.position)
