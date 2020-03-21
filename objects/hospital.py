7f8ad30c331e540c7f832faaedc6174e8825c341
jp_172@web.de <julius@takemobi.com>
2020-03-20 21:34:00 -0400

    reordering stuff

--- objects/hospital.py
+++
@@ -1,6 +0,0 @@
-class Hospital:
-    def __init__(self, nbr_free_beds, nbr_free_corona_beds, lat, lon):
-        self.nbr_free_beds = nbr_free_beds
-        self.nbr_free_corona_beds = nbr_free_corona_beds
-        self.lat = lat
-        self.lon = lon
--- objects/vehicle.py
+++
@@ -1,6 +0,0 @@
-class Vehicle:
-    def __init__(self, max_range, is_corona_vehicle, lat, lon):
-        self.max_range = max_range
-        self.is_corona_vehicle = is_corona_vehicle
-        self.lat = lat
-        self.lon = lon
