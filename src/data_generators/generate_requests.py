1630800477bc50f19cf299687df1b1738569acd5
jp_172@web.de <julius@takemobi.com>
2020-03-21 11:56:32 -0400

    new request generator

---
+++ src/data_generators/generate_requests.py
@@ -0,0 +1,26 @@
+import json, random
+
+from .generate_patients import generate_patients
+from ..objects.request import Request
+
+# range where the request file time is taken from
+time_range = (0, 100000)
+
+def generate_requests(write = True):
+    patients = generate_patients(write = False)
+
+    reqs = {}
+
+    for ident, p in patients.items():
+        r = Request(
+            ident = ident,
+            person = p,
+            filed_at = random.uniform(time_range[0], time_range[1])
+        )
+        reqs[ident] = r.to_dict()
+
+    if write:
+        with open("data/patient_requests/requests.json", "w") as f:
+            json.dump(reqs, f)
+
+    return reqs
