f1466e39c0c697fc4cf10bed63b7f4f2804b473d
helthazar <helthazar@gmail.com>
2020-03-22 01:45:49 +0100

    Add parse cities to generate patients.

--- src/data_generators/generate_patients.py
+++ src/data_generators/generate_patients.py
@@ -1,4 +1,5 @@
 # Generates a JSON file with @nbr_patients patients where for each of them a request will be filed
+import csv
 import json
 import random
 
@@ -9,7 +10,26 @@ from ..objects.position import Position
 nbr_patients = 20000
 
 
+def parse_cities():
+    cities = {}
+    with open('data/data_gatherer/full_population_data.csv', newline='') as csvfile:
+        city_reader = csv.reader(csvfile)
+        for row in city_reader:
+            city = City(
+                    ident=row[0], 
+                    name=row[1], 
+                    position=Position(row[3], row[4]), 
+                    population=row[5], 
+                    state=row[6])
+            cities[city.ident] = city
+    return cities
+
+
 def generate_patients(write=True):
+    cities = parse_cities()
+    for city in cities.values():
+        print(city.ident, city.name, city.position.lat, city.position.lon, city.population, city.state)
+
     # coordinate ranges
     lat_range = (48, 52)
     lon_range = (8, 12)
