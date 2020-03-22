f1a6052f5f9bb95d59e0a97c15642aa3b4948cfb
helthazar <helthazar@gmail.com>
2020-03-22 02:57:05 +0100

    Small refactor changes to generate patients.

--- src/data_generators/generate_patients.py
+++ src/data_generators/generate_patients.py
@@ -7,12 +7,13 @@ from ..objects.city import City
 from ..objects.person import Person
 from ..objects.position import Position
 
-nbr_patients = 20000
+nbr_patients = 20
 lon_delta = 0.1
 lat_delta = 0.1
 
 def parse_cities():
     cities = {}
+
     with open('data/data_gatherer/full_population_data.csv', newline='') as csvfile:
         city_reader = csv.reader(csvfile)
         for row in city_reader:
@@ -23,6 +24,7 @@ def parse_cities():
                     population=int(row[5]), 
                     state=row[6])
             cities[city.ident] = city
+
     return cities
 
 
@@ -31,7 +33,9 @@ def sample_patients(cities):
             population=list(cities.values()),
             weights=[city.population for city in cities.values()],
             k=nbr_patients)
+
     patients = {}
+
     for i, patient_city in enumerate(patient_cities):
         patient = Person(
             ident=i,
@@ -43,13 +47,17 @@ def sample_patients(cities):
             severity=random.random()
         )
         patients[patient.ident] = patient.to_dict()
+
     return patients
 
 
 def generate_patients(write=True):
     cities = parse_cities()
+
     patients = sample_patients(cities)
+
     if write:
         with open("data/patient_requests/patients.json", "w") as f:
             json.dump(patients, f)
+
     return patients 
