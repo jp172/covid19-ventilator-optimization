98f7796153e0deb5e4aa167940eb62ef7c202ebb
marco-botte <marco.botte90@googlemail.com>
2020-03-22 12:16:16 +0100

    Actually add global variable file

---
+++ src/globals.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+UPDATE_CASES = ["normal_to_corona", "corona_leaves_hosp"]
+
+CAPACITY_SCALAR = 0.25
+
+
+class ScoreWeight(Enum):
+    WEIGHT_OVER_10 = 5
+    WEIGHT_OVER_5 = 2
+    WEIGHT_OVER_1 = 1
+
+
+class Scenario(Enum):
+    WORST = "worst"
+    HIGH = "high"
+    NORMAL = "normal"
