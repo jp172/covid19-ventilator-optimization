da59b39650265dff8c0d5f0a1c3c5d7107b03437
helthazar <helthazar@gmail.com>
2020-03-22 04:13:06 +0100

    Update write helper function.

--- src/helper_functions/write_data.py
+++ src/helper_functions/write_data.py
@@ -1,7 +1,11 @@
 import json
 
 
-def write_object_dict(object_dict, output_file):
+def write_dict(output_dict, output_file):
     with open(output_file, "w") as f:
-        object_dict = dict((key, value.to_dict()) for key, value in object_dict.items())
         json.dump(object_dict, f)
+
+
+def write_object_dict(object_dict, output_file):
+    object_dict = dict((key, value.to_dict()) for key, value in object_dict.items())
+    write_dict(object_dict)
