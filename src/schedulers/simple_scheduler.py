f5e736b6a3b8e8293cf6b8d0cbede493e68de823
marco-botte <marco.botte90@googlemail.com>
2020-03-22 01:24:30 +0100

    Adapt scheduler for non feasible cases

--- src/schedulers/simple_scheduler.py
+++ src/schedulers/simple_scheduler.py
@@ -7,13 +7,16 @@ class SimpleScheduler:
     # disponents schedule patients to bed as long as there are available beds in nearby hospitals
     # they choose the nearest available hospital
     def assign_request(self, hospitals, request, max_vehicle_range):
-        feasible_hospitals = get_feasible_hospitals(
-            hospitals, request.person.position, max_vehicle_range
-        )
-
         sorted_hospitals = sorted(
-            feasible_hospitals,
+            hospitals,
             key=lambda hosp: get_distance(hosp.position, request.person.position),
         )
 
-        return RankedProposal(sorted_hospitals[:3])
+        feasible_hospitals = get_feasible_hospitals(
+            sorted_hospitals, request.person.position, max_vehicle_range
+        )
+
+        if not feasible_hospitals:
+            return RankedProposal([sorted_hospitals[0]])
+
+        return RankedProposal(feasible_hospitals[: min(len(feasible_hospitals), 3)])
