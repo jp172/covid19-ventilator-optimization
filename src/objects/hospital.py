6e45ec1e82603804c3e4f577c1f70943de66fd6e
marco-botte <marco.botte90@googlemail.com>
2020-03-22 18:44:08 +0100

    Remove some comments

--- src/objects/hospital.py
+++ src/objects/hospital.py
@@ -18,11 +18,6 @@ class Hospital:
     # nbr_corona_beds: int ### needed later in generator !
 
     def calculate_capacity_coefficient(self):
-        # if self.nbr_free_corona_beds > NUMBER_FREE_CORONA_BEDS:
-        #     print("Attention: negative capacity. The calculation is fukdup.")
-        # self.capacity_coefficient = 1 - (
-        #     max(0, self.nbr_free_corona_beds / NUMBER_FREE_CORONA_BEDS)
-        # )
         self.capacity_coefficient = (
             (1 + CAPACITY_SCALAR * self.nbr_corona_pat_in_normal_bed)
         ) / (1 + max(-0.75, self.nbr_free_corona_beds))
